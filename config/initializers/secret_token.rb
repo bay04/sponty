bcf545eab34ddba310429d73e3b677c26011bdfb
bay04 <MoosieMAC@Mostafas-iMac.local>
2013-08-20 17:00:43 -0400

    Spont stage 1___Just Bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spont::Application.config.secret_key_base = '725d3fc9ae74a4c8abb72819cc7a9808e0300c558752950b3d485364a16c52ef2f938b9a51f14fbf17d86247fec0e0576dd50d7f4450885c3dc5357a59f3d117'
